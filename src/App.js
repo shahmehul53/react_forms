d4e13dd4605bb1c65dffef71514b38e1879934ce
shahmehul-BV <mehul.shah@brainvire.com>
2022-03-15 16:30:15 +0530

    Hooks Validation

--- src/App.js
+++ src/App.js
@@ -1,62 +1,22 @@
 import { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
 import "./App.css";
 
 function App() {
-  const initialValues = { username: "", email: "", password: "" };
-  const [formValues, setFormValues] = useState(initialValues);
-  const [formErrors, setFormErrors] = useState({});
-  const [isSubmit, setIsSubmit] = useState(false);
+  const { register, handleSubmit, errors } = useForm();
+  const [userInfo, setUserInfo] = useState();
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormValues({ ...formValues, [name]: value });
-    setIsSubmit(true);
+  const onSubmit = (data) => {
+    setUserInfo(data);
+    console.log("data==>", data);
   };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setFormErrors(validate(formValues));
-  };
-
-  useEffect(() => {
-    console.log("errors", formErrors);
-    if (Object.keys(formErrors).length === 0 && isSubmit) {
-      console.log("formValues", formValues);
-    }
-  }, [formErrors]);
-
-  const validate = (values) => {
-    const errors = {};
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
-    if (!values.username) {
-      errors.username = "Username is required";
-    }
-    if (!values.email) {
-      errors.email = "email is required";
-    } else if (!regex.test(values.email)) {
-      errors.email = "This is not a valid email format";
-    }
-    if (!values.password) {
-      errors.password = "password is required";
-    } else if (values.password.length < 4) {
-      errors.password = "Password must be more than 4 characters";
-    } else if (values.password.length > 10) {
-      errors.password = "Password cannot exceed more than 10 characters";
-    }
-
-    return errors;
-  };
-
+  console.log("errors", errors);
   return (
     <div className="container">
-      {Object.keys(formErrors).length === 0 && isSubmit ? (
-        <div className="ui message success"> Signed in Successfully </div>
-      ) : (
-        <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
-      )}
+      <pre>{JSON.stringify(userInfo, undefined, 2)}</pre>
 
-      <form onSubmit={handleSubmit}>
-        <h1>Login Form</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h1>Registration Form</h1>
         <div className="ui divider"></div>
         <div className="ui form">
           <div className="field">
@@ -65,33 +25,46 @@ function App() {
               type="text"
               name="username"
               placeholder="Username"
-              value={formValues.username}
-              onChange={handleChange}
+              ref={register({ required: "Username is required" })}
             />
           </div>
-          <p>{formErrors.username}</p>
+          <p>{errors.username?.message}</p>
           <div className="field">
             <label>Email</label>
             <input
               type="text"
               name="email"
               placeholder="Email"
-              value={formValues.email}
-              onChange={handleChange}
+              ref={register({
+                required: "Email is required",
+                pattern: {
+                  value: /^\S+@\S+$/i,
+                  message: "This is not a valid email",
+                },
+              })}
             />
           </div>
-          <p>{formErrors.email}</p>
+          <p>{errors.email?.message}</p>
           <div className="field">
             <label>Password</label>
             <input
               type="password"
               name="password"
               placeholder="Password"
-              value={formValues.password}
-              onChange={handleChange}
+              ref={register({
+                required: "Password is required",
+                minLength: {
+                  value: 4,
+                  message: "Password must be more than 4 charcters",
+                },
+                maxLength: {
+                  value: 10,
+                  message: "Password cannot exceed more than 10 characters"
+                }
+              })}
             />
           </div>
-          <p>{formErrors.password}</p>
+          <p>{errors.password?.message}</p>
           <button className="fluid ui button blue">Submit</button>
         </div>
       </form>
